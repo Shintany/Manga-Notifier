80e6627a672cedb5db0b9a85c30334fda8afc554
Shin <irchadtuankitchil@gmail.com>
2018-11-12 22:58:40 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Author : TUAN KITCHIL Irchad
+# Python : 3
+
+# This program purpose is to send a mail with the newest chapter of the manga list
+# which appear in the .csv file (list.csv)
+
+import bs4
+import urllib
+
+if __name__ == '__main__':
+    
+else:
+    print('main.py file is not supposed to be call by another program....')
+    quit()
